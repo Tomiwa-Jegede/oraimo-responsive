35d125ba8c3400220698b46908dcad7a941ebaae
Tomiwa-Jegede <jegedetomiwa1@gmail.com>
2024-07-20 09:46:35 -0700

    completed the top and the bottom navbar here

---
+++ oraimoJS/productDropDownItems.js
@@ -0,0 +1,72 @@
+export const productImage = [
+  {
+    title: 'Power',
+    image: '/oraimoIMAGE/productdropdownimages/powerbank.webp',
+    list: {
+      nos1: 'Power Banks',
+      nos2: 'Wall Chargers',
+      nos3: 'Power Strips',
+      nos4: 'Solar Generators',
+      nos5: 'Car Chargers',
+      nos6: 'Cables',
+    },
+    sale:'HOT',
+    class:'tag'
+  },
+  {
+    title: 'Audio',
+    image: '/oraimoIMAGE/productdropdownimages/earbuds.webp',
+    list: {
+      nos1: 'wireless Stereo EarBuds',
+      nos2: 'Wireless OverEar Headphones',
+      nos3: 'Wearless Neckband Headphones',
+      nos4: 'Opem-Ear Headphones',
+      nos5: 'wireless Speakers',
+      nos6: 'Wired Earphones',
+    },
+    sale:'HOT',
+    class:'tag'
+  },
+  {
+    title: 'Smart & Offices',
+    image: '/oraimoIMAGE/productdropdownimages/smartwatch.webp',
+    list: {
+      nos1: 'Smart Watches',
+      nos2: 'Smart Scales',
+      nos3: 'Smart Lighting',
+      nos4: 'Mouse & KeyBoard',
+      nos5: 'Camera Accessories',
+      nos6: 'BackPacks',
+    },
+    sale:'HOT',
+    class:'tag'
+  },
+  {
+    title: 'Pesonal Care',
+    image: '/oraimoIMAGE/productdropdownimages/clipper.webp alt=',
+    list: {
+      nos1: 'Grooming Series',
+      nos2: 'Oral Care',
+      nos3: 'Mirrors',
+      nos4: 'Hair Dryers',
+      nos5: 'Hair Styling Tools',
+      nos6: ''
+    },
+    sale:'',
+    class:'tag1'
+  },
+  {
+    title: 'Home Appliances',
+    image: '/oraimoIMAGE/productdropdownimages/vacuum.webp',
+    list: {
+      nos1: 'Vacuums',
+      nos2: 'Blenders',
+      nos3: 'Handheld fans',
+      nos4: 'Humidifier',
+      nos5: 'Electric Kettles',
+      nos6: ''
+    },
+    sale:'',
+    class:'tag1'
+  }
+]
